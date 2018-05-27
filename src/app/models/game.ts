8e4ec51b7796552ce8ff3121e1e8d2d8a8fa0e17
myrionsc <marpht23@gmail.com>
2018-05-27 19:51:56 +0200

    added phaser template

---
+++ src/app/models/game.ts
@@ -0,0 +1,37 @@
+/**
+ * @author       Digitsensitive <digit.sensitivee@gmail.com>
+ * @copyright    2018 Digitsensitive
+ * @description  Coin Runner
+ * @license      Digitsensitive
+ */
+
+import "phaser";
+import { GameScene } from "./scenes/gameScene";
+
+const config: GameConfig = {
+  title: "Coin Runner",
+  url: "https://github.com/digitsensitive/phaser3-typescript",
+  version: "1.1",
+  width: 768,
+  height: 576,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: [GameScene],
+  input: {
+    keyboard: true,
+    mouse: false,
+    touch: false,
+    gamepad: false
+  },
+  backgroundColor: "#3A99D9"
+};
+
+export class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+  }
+}
+
+// window.onload = () => {
+//   const game = new Game();
+// };
