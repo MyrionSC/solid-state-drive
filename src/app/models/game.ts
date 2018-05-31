b0013c41b31d62f6f40a163777a448b3b912c254
myrionsc <marpht23@gmail.com>
2018-05-31 16:37:50 +0200

    reformats

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -5,31 +5,31 @@
  * @license      Digitsensitive
  */
 
-import "phaser";
-import { GameScene } from "./scenes/gameScene";
+import 'phaser';
+import {GameScene} from './scenes/gameScene';
 
 const config: GameConfig = {
-  title: "Coin Runner",
-  url: "https://github.com/digitsensitive/phaser3-typescript",
-  version: "1.1",
-  width: 768,
-  height: 576,
-  type: Phaser.AUTO,
-  parent: "game",
-  scene: [GameScene],
-  input: {
-    keyboard: true,
-    mouse: false,
-    touch: false,
-    gamepad: false
-  },
-  backgroundColor: "#3A99D9"
+    title: 'Coin Runner',
+    url: 'https://github.com/digitsensitive/phaser3-typescript',
+    version: '1.1',
+    width: 768,
+    height: 576,
+    type: Phaser.AUTO,
+    parent: 'game',
+    scene: [GameScene],
+    input: {
+        keyboard: true,
+        mouse: false,
+        touch: false,
+        gamepad: false
+    },
+    backgroundColor: '#3A99D9'
 };
 
 export class Game extends Phaser.Game {
-  constructor() {
-    super(config);
-  }
+    constructor() {
+        super(config);
+    }
 }
 
 // window.onload = () => {
